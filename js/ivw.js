bbded2e1c14711b067cb6abf2c4bc30132bab5f8
Daniel Bosen <daniel@bosen.eu>
2016-02-22 13:04:21 +0100

    fix iphone calls

--- js/ivw.js
+++
@@ -1,9 +0,0 @@
-if (window.innerWidth < ivw_mobile_width) {
-    iam_data.cp = ivw_cpm;
-    if(ivw_mobile_site) {
-        iam_data.st = ivw_mobile_site;
-    }
-}
-if ((typeof window.iom !== 'undefined') && (typeof window.iom.c === 'function')) {
-    iom.c(iam_data, 1);
-}
