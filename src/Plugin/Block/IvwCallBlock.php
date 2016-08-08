784ade8143e6d8deb4b582262d2a850b986beef5
Daniel Bosen <daniel.bosen@burda.com>
2016-08-08 12:12:13 +0200

    move settings to block

--- src/Plugin/Block/IvwCallBlock.php
+++ src/Plugin/Block/IvwCallBlock.php
@@ -2,12 +2,13 @@
 
 namespace Drupal\ivw_integration\Plugin\Block;
 
-use Drupal\ivw_integration\IvwTracker;
-use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\ivw_integration\IvwTracker;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -27,6 +28,13 @@ class IvwCallBlock extends BlockBase implements ContainerFactoryPluginInterface 
   protected $ivwTracker;
 
   /**
+   * The config factory object.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
    * Constructs an Related Content object.
    *
    * @param array $configuration
@@ -37,15 +45,19 @@ class IvwCallBlock extends BlockBase implements ContainerFactoryPluginInterface 
    *   The plugin implementation definition.
    * @param \Drupal\ivw_integration\IvwTracker $ivw_tracker
    *   The ivw tracker object.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory object.
    */
   public function __construct(
     array $configuration,
     $plugin_id,
     $plugin_definition,
-    IvwTracker $ivw_tracker
+    IvwTracker $ivw_tracker,
+    ConfigFactoryInterface $config_factory
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->ivwTracker = $ivw_tracker;
+    $this->configFactory = $config_factory;
   }
 
   /**
@@ -61,7 +73,8 @@ class IvwCallBlock extends BlockBase implements ContainerFactoryPluginInterface 
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('ivw_integration.tracker')
+      $container->get('ivw_integration.tracker'),
+      $container->get('config.factory')
     );
   }
 
@@ -70,15 +83,29 @@ class IvwCallBlock extends BlockBase implements ContainerFactoryPluginInterface 
    */
   public function build() {
     $tracker = $this->ivwTracker->getTrackingInformation();
+    $config = $this->configFactory->get('ivw_integration.settings');
 
     // Site is missing, do not render tag.
     if (empty($tracker['st'])) {
       return [];
     }
 
+
+    $mobile_width = $config->get("mobile_width") ? $config->get("mobile_width") : '';
+    $mobile_site = $config->get("mobile_site") ? $config->get("mobile_site") : '';
+    $mobile_sv = $tracker['mobile_sv'];
+
     return array(
       'ivw_call' => array(
         '#theme' => 'ivw_call',
+        '#st' =>  $tracker['st'],
+        '#cp' =>  $tracker['cp'],
+        '#sv' =>  $tracker['sv'],
+        '#co' => '', # not yet configurable
+        '#mobile_cp' => $tracker['cpm'],
+        '#mobile_st' => $mobile_site,
+        '#mobile_sv' => $mobile_sv,
+        '#mobile_width' => $mobile_width
       ),
     );
   }
