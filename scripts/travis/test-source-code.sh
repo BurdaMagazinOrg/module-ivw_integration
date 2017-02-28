e8a05947dd8409bf4248c5ad5e4c792854190dd5
Daniel Bosen <daniel.bosen@burda.com>
2017-02-28 10:22:55 +0100

    remove double space

--- scripts/travis/test-source-code.sh
+++ scripts/travis/test-source-code.sh
@@ -7,7 +7,7 @@ composer global require drupal/coder
 # run phpcs
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme -p .
-phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme  -p .
+phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme -p .
 
 # JS ESLint checking
 mv ~/.nvm ~/.nvm-backup
